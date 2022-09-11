64d18dcdffe08fa57c2d2fec2ab6f5c77c687f3e
Sulthan Daffa <sdaffa568@gmail.com>
2022-09-11 13:50:14 +0700

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// ----- preloader -----
+
+function preloaded(){
+    document.getElementById("preloader").style.display = "none"
+}
+
+setTimeout(
+    function(){
+        preloaded();
+    },
+    1500
+)
+
+// ----- preload animate -----
+
